ebcd3fd3907b093d0c605877f9760365bd454970
cjscholten <cjscholten@gmail.com>
2016-07-24 21:45:22 +0200

    Update bootrepair.sh

--- bootrepair.sh
+++ bootrepair.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository -y ppa:yannubuntu/boot-repair
 sudo add-apt-repository -y ppa:danielrichter2007/grub-customizer
 sudo apt-get update
-sudo apt-get install -y boot-repair && boot-repair
+sudo apt-get install -y boot-repair 
 sudo apt-get install -y grub-customizer
